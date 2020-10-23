1375e8f7775ffcf62fc1ea15585edaf3783f0c20
nicokuki96 <72584691+nicokuki96@users.noreply.github.com>
2020-10-23 18:17:54 -0300

    actualizacion de js

--- assets/js/mapin.js
+++ assets/js/mapin.js
@@ -1,17 +1,26 @@
 $(document).ready(function () {
-    function changeTab(e) {
-      e.preventDefault();
-      $(".pin").removeClass("drop-it");
-      $(".tooltip").removeClass("visible");
-      $(this).addClass("active");
-      focus($(this).attr("href"));
-    }
-  
-    function focus(area) {
-      $(".layer").removeClass("top");
-      $(area).addClass("top").removeClass("faded");
-      $(area).children(".pin").addClass("drop-it top");
-    }
-  
-    $(".controls a").on("click", changeTab);
+  function changeTab(e) {
+    e.preventDefault();
+    $(".pin").removeClass("drop-it");
+    $(".tooltip").removeClass("visible");
+    $(this).addClass("active");
+    focus($(this).attr("href"));
+  }
+
+  function focus(area) {
+    $(".layer").removeClass("top");
+    $(area).addClass("top").removeClass("faded");
+    $(area).children(".pin").addClass("drop-it top");
+  }
+
+  $(".controls a").on("click", changeTab);
+});
+
+  $("#okmap").click(function(){
+    $("#okmap").hide();
+    $(".textmap").hide();
+    $(".subtextmap").hide();
+    $(".textmapMobile").hide();
+    document.getElementById("mapdesktop").style.opacity = "1";
+    document.getElementById("mapmobile").style.opacity = "1";
   });
--- assets/js/slider_view_all.js
+++ assets/js/slider_view_all.js
@@ -1,99 +1,189 @@
-var $slider = $(".slideshow .slider"),
-  maxItems = $(".item", $slider).length,
-  dragging = false,
-  tracking,
-  rightTracking;
+$(document).ready(function () {
+  const $cont = $('.cont');
+  const $slider = $('.slider');
+  const $nav = $('.nav');
+  const winW = $(window).width();
+  const animSpd = 750; // Change also in CSS
+  const distOfLetGo = winW * 0.2;
+  let curSlide = 1;
+  let animation = false;
+  let autoScrollVar = true;
+  let diff = 0;
 
-$sliderRight = $(".slideshow")
-  .clone()
-  .addClass("slideshow-right")
-  .appendTo($(".split-slideshow"));
+  // Generating slides
+  let arrCities = ['Chad', 'Cameroon', 'New—York', 'Singapore', 'Prague']; // Change number of slides in CSS also
+  let numOfCities = arrCities.length;
+  let arrCitiesDivided = [];
 
-rightItems = $(".item", $sliderRight).toArray();
-reverseItems = rightItems.reverse();
-$(".slider", $sliderRight).html("");
-for (i = 0; i < maxItems; i++) {
-  $(reverseItems[i]).appendTo($(".slider", $sliderRight));
-}
+  arrCities.map(city => {
+    let length = city.length;
+    let letters = Math.floor(length / 4);
+    let exp = new RegExp(".{1," + letters + "}", "g");
 
-$slider.addClass("slideshow-left");
-$(".slideshow-left")
-  .slick({
-    vertical: true,
-    verticalSwiping: true,
-    arrows: false,
-    infinite: true,
-    dots: true,
-    speed: 1000,
-    cssEase: "cubic-bezier(0.7, 0, 0.3, 1)"
-  })
-  .on("beforeChange", function (event, slick, currentSlide, nextSlide) {
-    if (
-      currentSlide > nextSlide &&
-      nextSlide == 0 &&
-      currentSlide == maxItems - 1
-    ) {
-      $(".slideshow-right .slider").slick("slickGoTo", -1);
-      $(".slideshow-text").slick("slickGoTo", maxItems);
-    } else if (
-      currentSlide < nextSlide &&
-      currentSlide == 0 &&
-      nextSlide == maxItems - 1
-    ) {
-      $(".slideshow-right .slider").slick("slickGoTo", maxItems);
-      $(".slideshow-text").slick("slickGoTo", -1);
-    } else {
-      $(".slideshow-right .slider").slick(
-        "slickGoTo",
-        maxItems - 1 - nextSlide
-      );
-      $(".slideshow-text").slick("slickGoTo", nextSlide);
-    }
-  })
-  .on("mousewheel", function (event) {
-    event.preventDefault();
-    if (event.deltaX > 0 || event.deltaY < 0) {
-      $(this).slick("slickNext");
-    } else if (event.deltaX < 0 || event.deltaY > 0) {
-      $(this).slick("slickPrev");
-    }
-  })
-  .on("mousedown touchstart", function () {
-    dragging = true;
-    tracking = $(".slick-track", $slider).css("transform");
-    tracking = parseInt(tracking.split(",")[5]);
-    rightTracking = $(".slideshow-right .slick-track").css("transform");
-    rightTracking = parseInt(rightTracking.split(",")[5]);
-  })
-  .on("mousemove touchmove", function () {
-    if (dragging) {
-      newTracking = $(".slideshow-left .slick-track").css("transform");
-      newTracking = parseInt(newTracking.split(",")[5]);
-      diffTracking = newTracking - tracking;
-      $(".slideshow-right .slick-track").css({
-        transform:
-          "matrix(1, 0, 0, 1, 0, " + (rightTracking - diffTracking) + ")"
-      });
-    }
-  })
-  .on("mouseleave touchend mouseup", function () {
-    dragging = false;
+    arrCitiesDivided.push(city.match(exp));
   });
 
-$(".slideshow-right .slider").slick({
-  swipe: false,
-  vertical: true,
-  arrows: false,
-  infinite: true,
-  speed: 950,
-  cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
-  initialSlide: maxItems - 1
+  let generateSlide = function (city) {
+    let frag1 = $(document.createDocumentFragment());
+    let frag2 = $(document.createDocumentFragment());
+    const numSlide = arrCities.indexOf(arrCities[city]) + 1;
+    const firstLetter = arrCitiesDivided[city][0].charAt(0);
+
+    const $slide =
+    $(`<div data-target="${numSlide}" class="slide slide--${numSlide}">
+							<div class="slide__darkbg slide--${numSlide}__darkbg"></div>
+							<div class="slide__text-wrapper slide--${numSlide}__text-wrapper"></div>
+						</div>`);
+
+    const letter =
+    $(`<div class="slide__letter slide--${numSlide}__letter">
+							${firstLetter}
+						</div>`);
+
+    for (let i = 0, length = arrCitiesDivided[city].length; i < length; i++) {
+      const text =
+      $(`<div class="slide__text slide__text--${i + 1}">
+								${arrCitiesDivided[city][i]}
+							</div>`);
+      frag1.append(text);
+    }
+
+    const navSlide = $(`<li data-target="${numSlide}" class="nav__slide nav__slide--${numSlide}"></li>`);
+    frag2.append(navSlide);
+    $nav.append(frag2);
+
+    $slide.find(`.slide--${numSlide}__text-wrapper`).append(letter).append(frag1);
+    $slider.append($slide);
+
+    if (arrCities[city].length <= 4) {
+      $('.slide--' + numSlide).find('.slide__text').css("font-size", "12vw");
+    }
+  };
+
+  for (let i = 0, length = numOfCities; i < length; i++) {
+    generateSlide(i);
+  }
+
+  $('.nav__slide--1').addClass('nav-active');
+
+  // Navigation
+  function bullets(dir) {
+    $('.nav__slide--' + curSlide).removeClass('nav-active');
+    $('.nav__slide--' + dir).addClass('nav-active');
+  }
+
+  function timeout() {
+    animation = false;
+  }
+
+  function pagination(direction) {
+    animation = true;
+    diff = 0;
+    $slider.addClass('animation');
+    $slider.css({
+      'transform': 'translate3d(-' + (curSlide - direction) * 100 + '%, 0, 0)' });
+
+
+    $slider.find('.slide__darkbg').css({
+      'transform': 'translate3d(' + (curSlide - direction) * 50 + '%, 0, 0)' });
+
+
+    $slider.find('.slide__letter').css({
+      'transform': 'translate3d(0, 0, 0)' });
+
+
+    $slider.find('.slide__text').css({
+      'transform': 'translate3d(0, 0, 0)' });
+
+  }
+
+  function navigateRight() {
+    if (!autoScrollVar) return;
+    if (curSlide >= numOfCities) return;
+    pagination(0);
+    setTimeout(timeout, animSpd);
+    bullets(curSlide + 1);
+    curSlide++;
+  }
+
+  function navigateLeft() {
+    if (curSlide <= 1) return;
+    pagination(2);
+    setTimeout(timeout, animSpd);
+    bullets(curSlide - 1);
+    curSlide--;
+  }
+
+  function toDefault() {
+    pagination(1);
+    setTimeout(timeout, animSpd);
+  }
+
+  // Events
+  $(document).on('mousedown touchstart', '.slide', function (e) {
+    if (animation) return;
+    let target = +$(this).attr('data-target');
+    let startX = e.pageX || e.originalEvent.touches[0].pageX;
+    $slider.removeClass('animation');
+
+    $(document).on('mousemove touchmove', function (e) {
+      let x = e.pageX || e.originalEvent.touches[0].pageX;
+      diff = startX - x;
+      if (target === 1 && diff < 0 || target === numOfCities && diff > 0) return;
+
+      $slider.css({
+        'transform': 'translate3d(-' + ((curSlide - 1) * 100 + diff / 30) + '%, 0, 0)' });
+
+
+      $slider.find('.slide__darkbg').css({
+        'transform': 'translate3d(' + ((curSlide - 1) * 50 + diff / 60) + '%, 0, 0)' });
+
+
+      $slider.find('.slide__letter').css({
+        'transform': 'translate3d(' + diff / 60 + 'vw, 0, 0)' });
+
+
+      $slider.find('.slide__text').css({
+        'transform': 'translate3d(' + diff / 15 + 'px, 0, 0)' });
+
+    });
+  });
+
+  $(document).on('mouseup touchend', function (e) {
+    $(document).off('mousemove touchmove');
+
+    if (animation) return;
+
+    if (diff >= distOfLetGo) {
+      navigateRight();
+    } else if (diff <= -distOfLetGo) {
+      navigateLeft();
+    } else {
+      toDefault();
+    }
+  });
+
+  $(document).on('click', '.nav__slide:not(.nav-active)', function () {
+    let target = +$(this).attr('data-target');
+    bullets(target);
+    curSlide = target;
+    pagination(1);
+  });
+
+  $(document).on('click', '.side-nav', function () {
+    let target = $(this).attr('data-target');
+
+    if (target === 'right') navigateRight();
+    if (target === 'left') navigateLeft();
+  });
+
+  $(document).on('keydown', function (e) {
+    if (e.which === 39) navigateRight();
+    if (e.which === 37) navigateLeft();
+  });
+
 });
-$(".slideshow-text").slick({
-  swipe: false,
-  vertical: true,
-  arrows: false,
-  infinite: true,
-  speed: 900,
-  cssEase: "cubic-bezier(0.7, 0, 0.3, 1)"
-});
+
+
+
+
